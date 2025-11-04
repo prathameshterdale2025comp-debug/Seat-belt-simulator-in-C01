8e0ac0e5e1a1cdb0ab3206ea9fa19c3e6b4bbfff
prathameshterdale2025comp-debug <prathameshterdale2025.comp@mmcoe.edu.in>
2025-11-04 09:37:53 +0530

    Add files via upload

--- prathamesh.c
+++ prathamesh.c
@@ -1,14 +1,35 @@
-#include<stdio.h>
-int main(){
-int a;
-printf("Welcome dear passenger\n");
-printf("Enter 1 or 0 to plug in and plug out the seat belt\n");
-scanf("%d",&a);
-if(a==1){
-printf("Allright start your ride");
+#include <stdio.h>
+int main() {
+      char Name[100];
+    printf("Name:-");
+    scanf("%s",Name);
+    char Date[100];
+    printf("Date:");
+    scanf("%s",Date);
+    char Bill_No[100];
+    printf("Bill No:");
+    scanf("%s",Bill_No);
+    char Time[100];
+    printf("Time:");
+    scanf("%s",Time);
+    printf("                                ||श्री महालक्ष्मी नमः||           \n");
+    printf ("                          chandekar jewellers        \n");
+    printf("            near laxmi road karvanagar  \n");
+    printf("                      tal havali dist pune pin 411052\n ");
+    printf("                           GST NO- 294DDPB8125K1\n\n\n");
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Shopping Bill~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    printf("Name:-%s",Name);
+    printf("                                  Date:%s",Date);
+    printf("\nBill_No:%s",Bill_No);
+    printf("                                   Time:%s",Time);
+    printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    printf("Sr-No   Items               price         qty          Total          \n");
+    printf("1)      	SILVER KALASH        100000           1               100000         \n");
+    printf("2)     SILVAR SAMAI              50000               1               50000           \n");
+    printf("3)      SILVAR IDLES             3000               1               3000          \n");
+    printf("4)   SILVAR AARTI SET             35000               1              35000         \n");
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    printf("                                               Grand Total==188000      \n");
+    return 0;
 }
-else{
-printf("Please plug in the seat belt");
-}
-return 0;
-}
+
